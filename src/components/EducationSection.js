0da1a02baf57606b0cd75158da3fba18c066fbfd
Brian Monge <bm0801389031@gmail.com>
2022-03-18 14:42:37 -0700

    complete function hook fork

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,113 +1,225 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Print, NoPrint } from 'react-easy-print';
 import TextSection from './TextSection';
 
-class EducationSection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editMode: true,
-            schoolName: '',
-            study: '',
-            from: '',
-            to: '',
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const EducationSection = (props) => {
+    const [educationInfo, setEducationInfo] = useState({
+        schoolName: '',
+        study: '',
+        from: '',
+        to: '',
+    });
+    const [editMode, setEditMode] = useState(true);
 
-    handleChange(e) {
+    const handleChange = (e) => {
         const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        this.setState((prevState) => {
-            return { editMode: !prevState.editMode };
+        setEducationInfo((prevInfo) => {
+            return { ...prevInfo, [name]: value }
         });
-    }
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEditMode((prevMode) => !prevMode);
+    };
 
-    render() {
-        const { editMode, schoolName, study, from, to } = this.state;
-        const { id, handleDelete } = this.props;
-        if (!editMode) {
-            return (
-                <TextSection
-                    schoolName={schoolName}
-                    study={study}
-                    from={from}
-                    to={to}
-                    handleEdit={this.handleSubmit}
-                />
-            );
-        }
+    const { schoolName, study, from, to} = educationInfo;
 
+    const { id, handleDelete } = props;
+    if (!editMode) {
         return (
-            <NoPrint>
-                <Print printOnly>
-                    <p className='warning'>Fill in the form.</p>
-                </Print>
-                <section>
-                    <form className='section' action='' onSubmit={this.handleSubmit}>
-                        <label htmlFor='schoolName'>
-                            <p>School Name:</p>
-                            <input
-                                type='text'
-                                placeholder='School Name'
-                                name='schoolName'
-                                id='schoolName'
-                                onChange={this.handleChange}
-                                value={schoolName}
-                                required
-                            />
-                        </label>
-                        <label>
-                            <p>Title of Study:</p>
-                            <input
-                                type='text'
-                                placeholder='Title of study'
-                                name='study'
-                                onChange={this.handleChange}
-                                value={study}
-                                required
-                            />
-                        </label>
-                        <label>
-                            <p>From:</p>
-                            <input
-                                type='date'
-                                name='from'
-                                placeholder='From'
-                                onChange={this.handleChange}
-                                value={from}
-                                required
-                            />
-                        </label>
-                        
-                        <label>
-                            <p>To:</p>
-                            <input
-                                type='date'
-                                name='to'
-                                placeholder='To'
-                                onChange={this.handleChange}
-                                value={to}
-                                required
-                            />
-                        </label>
-                        <button className='formBtn'>Save</button>
-                        <button
-                            className='formBtn'
-                            type='button'
-                            onClick={() => handleDelete('educationIds', id)}>
-                            Delete
-                        </button>
-                    </form>
-                </section>
-            </NoPrint>
+            <TextSection
+                schoolName={schoolName}
+                study={study}
+                from={from}
+                to={to}
+                handleEdit={handleSubmit}
+            />
         );
     }
+
+    return (
+        <NoPrint>
+            <Print printOnly>
+                <p className='warning'>Fill in the form.</p>
+            </Print>
+            <section>
+                <form className='section' action='' onSubmit={handleSubmit}>
+                    <label htmlFor='schoolName'>
+                        <p>School Name:</p>
+                        <input
+                            type='text'
+                            placeholder='School Name'
+                            name='schoolName'
+                            id='schoolName'
+                            onChange={handleChange}
+                            value={schoolName}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <p>Title of Study:</p>
+                        <input
+                            type='text'
+                            placeholder='Title of study'
+                            name='study'
+                            onChange={handleChange}
+                            value={study}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <p>From:</p>
+                        <input
+                            type='text'
+                            placeholder='Title of study'
+                            name='study'
+                            onChange={handleChange}
+                            value={study}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <p>From:</p>
+                        <input
+                            type='date'
+                            name='from'
+                            placeholder='From'
+                            onChange={handleChange}
+                            value={from}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <p>To:</p>
+                        <input
+                            type='date'
+                            name='to'
+                            placeholder='To'
+                            onChange={handleChange}
+                            value={to}
+                            required
+                        />
+                    </label>
+                    <button className='forBtn'>Save</button>
+                    <button
+                        className='formBtn'
+                        type='button'
+                        onClick={() => handleDelete('educationIds', id)}>
+                        Delete
+                    </button>
+                </form>
+            </section>
+        </NoPrint>
+    )
 }
 
 export default EducationSection
+
+// class EducationSection extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             editMode: true,
+//             schoolName: '',
+//             study: '',
+//             from: '',
+//             to: '',
+//         };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleChange(e) {
+//         const { name, value } = e.target;
+//         this.setState({ [name]: value });
+//     }
+
+//     handleSubmit(e) {
+//         e.preventDefault();
+//         this.setState((prevState) => {
+//             return { editMode: !prevState.editMode };
+//         });
+//     }
+
+//     render() {
+//         const { editMode, schoolName, study, from, to } = this.state;
+//         const { id, handleDelete } = this.props;
+//         if (!editMode) {
+//             return (
+//                 <TextSection
+//                     schoolName={schoolName}
+//                     study={study}
+//                     from={from}
+//                     to={to}
+//                     handleEdit={this.handleSubmit}
+//                 />
+//             );
+//         }
+
+//         return (
+//             <NoPrint>
+//                 <Print printOnly>
+//                     <p className='warning'>Fill in the form.</p>
+//                 </Print>
+//                 <section>
+//                     <form className='section' action='' onSubmit={this.handleSubmit}>
+//                         <label htmlFor='schoolName'>
+//                             <p>School Name:</p>
+//                             <input
+//                                 type='text'
+//                                 placeholder='School Name'
+//                                 name='schoolName'
+//                                 id='schoolName'
+//                                 onChange={this.handleChange}
+//                                 value={schoolName}
+//                                 required
+//                             />
+//                         </label>
+//                         <label>
+//                             <p>Title of Study:</p>
+//                             <input
+//                                 type='text'
+//                                 placeholder='Title of study'
+//                                 name='study'
+//                                 onChange={this.handleChange}
+//                                 value={study}
+//                                 required
+//                             />
+//                         </label>
+//                         <label>
+//                             <p>From:</p>
+//                             <input
+//                                 type='date'
+//                                 name='from'
+//                                 placeholder='From'
+//                                 onChange={this.handleChange}
+//                                 value={from}
+//                                 required
+//                             />
+//                         </label>
+                        
+//                         <label>
+//                             <p>To:</p>
+//                             <input
+//                                 type='date'
+//                                 name='to'
+//                                 placeholder='To'
+//                                 onChange={this.handleChange}
+//                                 value={to}
+//                                 required
+//                             />
+//                         </label>
+//                         <button className='formBtn'>Save</button>
+//                         <button
+//                             className='formBtn'
+//                             type='button'
+//                             onClick={() => handleDelete('educationIds', id)}>
+//                             Delete
+//                         </button>
+//                     </form>
+//                 </section>
+//             </NoPrint>
+//         );
+//     }
+// }
+
--- src/components/GeneralSection.js
+++ src/components/GeneralSection.js
@@ -1,62 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NoPrint, Print } from 'react-easy-print';
 import TextSection from './TextSection';
 
-class GeneralSection extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			editMode: true,
-			firstName: '',
-			lastName: '',
-			email: '',
-			phone: '',
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+const GeneralSection = () => {
+	const [personalInfo, setPersonalInfo] = useState({
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+	});
+	const [editMode, setEditMode] = useState(true);
 
-	handleChange(e) {
-		const { name, value } = e.target;
-		this.setState({ [name]: value });
-	}
-
-	handleSubmit(e) {
-		e.preventDefault();
-		this.setState((prevState) => {
-			return { editMode: !prevState.editMode };
+	const handleChange = (e) => {
+		const {name, value} = e.target;
+		setPersonalInfo((prevInfo) => {
+			let newInfo = { ...prevInfo, [name]: value };
+			return newInfo;
 		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setEditMode((prevMode) => !prevMode);
+	};
+
+	const { firstName, lastName, email, phone } = personalInfo;
+
+	if (!editMode) {
+		return (
+			<TextSection
+				firstName={firstName}
+				lastName={lastName}
+				email={email}
+				phone={phone}
+				handleEdit={handleSubmit}
+			/>
+		);
 	}
 
-	render() {
-		const { editMode, firstName, lastName, email, phone } = this.state;
-
-		if (!editMode) {
-			return (
-				<TextSection
-					firstName={firstName}
-					lastName={lastName}
-					email={email}
-					phone={phone}
-					handleEdit={this.handleSubmit}
-				/>
-			);
-		}
-
-		return (
-			<NoPrint>
-				<Print printOnly>
-					<p className='warning'>Fill in the form.</p>
+	return (
+		<NoPrint>
+			<Print printOnly>
+				<p className='warning'>Fill in the form.</p>
 				</Print>
 				<section className='generalSection'>
-					<form className='section' action='' onSubmit={this.handleSubmit}>
+					<form className='section' action='' onSubmit={handleSubmit}>
 						<label>
 							<p>First Name:</p>
 							<input
 								type='text'
 								placeholder='First Name'
 								name='firstName'
-								onChange={this.handleChange}
+								onChange={handleChange}
 								value={firstName}
 								required
 							/>
@@ -68,7 +63,7 @@ class GeneralSection extends React.Component {
 								type='text'
 								placeholder='Last Name'
 								name='lastName'
-								onChange={this.handleChange}
+								onChange={handleChange}
 								value={lastName}
 								required
 							/>
@@ -80,7 +75,7 @@ class GeneralSection extends React.Component {
 								type='email'
 								placeholder='example@gmail.com'
 								name='email'
-								onChange={this.handleChange}
+								onChange={handleChange}
 								value={email}
 								required
 							/>
@@ -91,7 +86,7 @@ class GeneralSection extends React.Component {
 								type='tel'
 								name='phone'
 								placeholder='123-456-7890'
-								onChange={this.handleChange}
+								onChange={handleChange}
 								value={phone}
 								required
 							/>
@@ -101,9 +96,127 @@ class GeneralSection extends React.Component {
 						</button>
 					</form>
 				</section>
-			</NoPrint>
-		);
-	}
-}
+		</NoPrint>
+	);
+};
+
 
 export default GeneralSection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class GeneralSection extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			editMode: true,
+// 			firstName: '',
+// 			lastName: '',
+// 			email: '',
+// 			phone: '',
+// 		};
+// 		this.handleChange = this.handleChange.bind(this);
+// 		this.handleSubmit = this.handleSubmit.bind(this);
+// 	}
+
+// 	handleChange(e) {
+// 		const { name, value } = e.target;
+// 		this.setState({ [name]: value });
+// 	}
+
+// 	handleSubmit(e) {
+// 		e.preventDefault();
+// 		this.setState((prevState) => {
+// 			return { editMode: !prevState.editMode };
+// 		});
+// 	}
+
+// 	render() {
+// 		const { editMode, firstName, lastName, email, phone } = this.state;
+
+// 		if (!editMode) {
+// 			return (
+// 				<TextSection
+// 					firstName={firstName}
+// 					lastName={lastName}
+// 					email={email}
+// 					phone={phone}
+// 					handleEdit={this.handleSubmit}
+// 				/>
+// 			);
+// 		}
+
+// 		return (
+// 			<NoPrint>
+// 				<Print printOnly>
+// 					<p className='warning'>Fill in the form.</p>
+// 				</Print>
+// 				<section className='generalSection'>
+// 					<form className='section' action='' onSubmit={this.handleSubmit}>
+// 						<label>
+// 							<p>First Name:</p>
+// 							<input
+// 								type='text'
+// 								placeholder='First Name'
+// 								name='firstName'
+// 								onChange={this.handleChange}
+// 								value={firstName}
+// 								required
+// 							/>
+// 						</label>
+
+// 						<label>
+// 							<p>Last Name:</p>
+// 							<input
+// 								type='text'
+// 								placeholder='Last Name'
+// 								name='lastName'
+// 								onChange={this.handleChange}
+// 								value={lastName}
+// 								required
+// 							/>
+// 						</label>
+
+// 						<label>
+// 							<p>Email:</p>
+// 							<input
+// 								type='email'
+// 								placeholder='example@gmail.com'
+// 								name='email'
+// 								onChange={this.handleChange}
+// 								value={email}
+// 								required
+// 							/>
+// 						</label>
+// 						<label>
+// 							<p>Phone Number:</p>
+// 							<input
+// 								type='tel'
+// 								name='phone'
+// 								placeholder='123-456-7890'
+// 								onChange={this.handleChange}
+// 								value={phone}
+// 								required
+// 							/>
+// 						</label>
+// 						<button className='formBtn generalBtn' type='submit'>
+// 							Save
+// 						</button>
+// 					</form>
+// 				</section>
+// 			</NoPrint>
+// 		);
+// 	}
+// }
+
